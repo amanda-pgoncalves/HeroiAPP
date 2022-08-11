fa57e5f6c5c1771660269943d35fa32cb38e3482
unknown <amandapinheirog@outlook.com>
2022-08-11 10:21:05 -0400

    Adicionando o projeto HeroiAPP no GitHub

---
+++ EFCore.Dominio/HeroiBatalha.cs
@@ -0,0 +1,13 @@
+﻿namespace EFCore.Dominio
+{
+    public class HeroiBatalha
+    {
+        public int HeroiId { get; set; }
+
+        public Heroi Heroi { get; set; }
+
+        public int BatalhaId { get; set; }
+
+        public Batalha Batalha { get; set; }
+    }
+}
